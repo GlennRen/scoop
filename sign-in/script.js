27917fa9a028a292699b88603f8e59c8a7671812
GlennRen <glenn.r.phs@gmail.com>
2015-04-12 01:33:40 -0400

    lmao
    lmao

--- sign-in/script.js
+++ sign-in/script.js
@@ -1,8 +1,9 @@
 console.log("HEY I\'M INJECTED")
 $(function(){
 	if ((jQuery("meta[name='og:type']") && jQuery("meta[name='og:type']").attr("content") == "article") || (jQuery("meta[property='og:type']") && jQuery("meta[property='og:type']").attr("content") == "article")){
-			$()
 		    var scoopFragment = chrome.extension.getURL("index.html");
-	    document.write(scoopFragment + '<iframe src="' + document.location.href + '" width=80% height=100%></iframe>')
+		    $.get(scoopFragment, function(data){
+		    	document.write(data + '<iframe src="' + document.location.href + '" width=78% height=100%></iframe>')
+		    })
 	}
 });
