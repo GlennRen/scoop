3c4e66b4ca5a7906f34cfe05806c73da3fcb42da
GlennRen <glenn.r.phs@gmail.com>
2015-04-11 20:11:25 -0400

    RFDFASDASDF
    ASDF

--- script.js
+++
@@ -1,2 +0,0 @@
-if ((jQuery("meta[name='og:type']") && jQuery("meta[name='og:type']").attr("content") == "article") || (jQuery("meta[property='og:type']") && jQuery("meta[property='og:type']").attr("content") == "article"))
-    jQuery("body").html("This is an article.");
