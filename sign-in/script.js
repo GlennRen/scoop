ebb8b5342253069aac08568ffbf7db70310470f1
Isaiah J Turner <github.com@isaiahjturner.com>
2015-04-11 22:09:08 -0400

    structure

--- sign-in/script.js
+++ sign-in/script.js
@@ -1,17 +1,8 @@
 console.log("HEY I\'M INJECTED")
 $(function(){
 	if ((jQuery("meta[name='og:type']") && jQuery("meta[name='og:type']").attr("content") == "article") || (jQuery("meta[property='og:type']") && jQuery("meta[property='og:type']").attr("content") == "article")){
-		
-		    var scoopFragment = 
-		    	'<div id="scoop">'
-		    	+ '<p id="title"><strong>Scoop</strong></p>'
-		    	+ '<div id="sign-info">'
-		    	+ '<p id="welcome"><strong>Welcome to Scoop!</strong></p>'
-		    	+ '<p id="reason">We need access to your Twitter Account so you can reply to comments.</p>'
-		    	
-		    	+ '</div>'
-		    	+ '</div>';
+			$()
+		    var scoopFragment = chrome.extension.getURL("index.html");
 	    document.write(scoopFragment + '<iframe src="' + document.location.href + '" width=80% height=100%></iframe>')
 	}
-
 });
