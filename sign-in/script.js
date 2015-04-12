505c1b9594d8d63ac6e861d11e6b4e78ba2cb7f2
GlennRen <glenn.r.phs@gmail.com>
2015-04-12 02:32:39 -0400

    asdf
    asdf

--- sign-in/script.js
+++ sign-in/script.js
@@ -3,7 +3,7 @@ $(function(){
 	if ((jQuery("meta[name='og:type']") && jQuery("meta[name='og:type']").attr("content") == "article") || (jQuery("meta[property='og:type']") && jQuery("meta[property='og:type']").attr("content") == "article")){
 		    var scoopFragment = chrome.extension.getURL("index.html");
 		    $.get(scoopFragment, function(data){
-		    	document.write(data + '<iframe src="' + document.location.href + '" width=78% height=100%></iframe>')
+		    	document.write(data + '<iframe src="' + document.location.href + '" width=500px height=100%></iframe>')
 		    })
 	}
 });
